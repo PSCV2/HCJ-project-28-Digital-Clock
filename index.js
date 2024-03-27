cfb7b98298f96a214d8c4f3cf65b924e32e837de
PSCV2 <98485676+PSCV2@users.noreply.github.com>
2024-03-27 23:43:36 +0000

    digi

---
+++ index.js
@@ -0,0 +1,12 @@
+const hourE1 = document.getElementById ("hour")
+const minuteE1 = document.getElementById ("minutes")
+const secondE1 = document.getElementById ("seconds")
+const ampmE1 = document.getElementById ("ampm")
+
+function updateClock(){
+    let h = new Date().getHours()
+    let m = new Date().getMinutes()
+    let s = new Date().seconds()
+    let ampm = "AM"
+
+}
